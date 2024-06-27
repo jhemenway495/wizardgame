800709142d5f26f6ea5b10deaba51695a130767e
James-Hemenway <72679847+jhemenway495@users.noreply.github.com>
2024-06-27 15:21:52 -0800

    Worked on dash

--- wizardgame/Assets/Scripts/PlayerController.cs
+++ wizardgame/Assets/Scripts/PlayerController.cs
@@ -24,8 +24,10 @@ public class PlayerController : MonoBehaviour
     Vector2 currentDirVelocity = Vector2.zero;
     [SerializeField] float jumpVelocity = 5.0f;
     int jumps = 1;
+    [SerializeField] float dashTime = 1.5f;
     [SerializeField] float dashVelocity = 24.0f;
     int dashes = 1;
+    bool isDashing;
     
 
 
@@ -37,22 +39,15 @@ public class PlayerController : MonoBehaviour
     CharacterController controller = null;
     void Start()
     {
-        
         //Gets character controller
         controller = GetComponent<CharacterController>();
         
         //Locks The coursor in place and makes it invisible
         if(lockCursor)
         {
-
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
-
         }
-        
-
-
-
     }
 
 
@@ -76,19 +71,22 @@ public class PlayerController : MonoBehaviour
         //Camera input application
         playerCamera.localEulerAngles = Vector3.right * cameraPitch;
         transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
-
-        
     }
 
     void UpdateMovement() 
     {
+
+        //Taking player input and smoothing;
         Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         targetDir.Normalize();
+
+        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
 
         if (controller.isGrounded)
         {
             velocityY = 0.0f;
             jumps = 1;
+            dashes = 1;
         
         }
         if (Input.GetKeyDown(KeyCode.Space) && jumps > 0)
@@ -97,22 +95,31 @@ public class PlayerController : MonoBehaviour
             jumps -= 1;
         
         }
-        if (Input.GetKeyDown(KeyCode.LeftAlt) && jumps > 0)
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt) && dashes > 0 && !isDashing)
         {
-            velocityY = 0.0f;
-            currentDir = targetDir * dashVelocity;
-
+            StartCoroutine(PlayerDash());
         }
-        
-
 
         velocityY += gravity * Time.deltaTime;
 
-        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
-        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * walkSpeed + Vector3.up * velocityY;
+        Vector3 velocity;
+
+        if (!isDashing)
+            velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * walkSpeed + Vector3.up * velocityY;
+        else
+            velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * dashVelocity;
 
         controller.Move(velocity * Time.deltaTime);
     
+    }
+
+    IEnumerator PlayerDash()
+    {
+        isDashing = true;
+        dashes -= 1;
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
     }
     
 
